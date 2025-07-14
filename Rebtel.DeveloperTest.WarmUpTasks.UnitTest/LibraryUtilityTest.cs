9be4f9a40ff1033494ab75200d20313c0846a330
ShahriarRahmanAUST <tiloknet@gmail.com>
2025-07-14 23:46:22 +0200

    Added warm-up tasks with tests

---
+++ Rebtel.DeveloperTest.WarmUpTasks.UnitTest/LibraryUtilityTest.cs
@@ -0,0 +1,43 @@
+﻿using WarmUpTasks;
+
+namespace Rebtel.DeveloperTest.WarmUpTasks.UnitTest
+{
+
+    [TestClass]
+    public sealed class LibraryUtilityTest
+    {
+        [TestMethod]
+        public void CheckIsBookIdPowerOfTwo()
+        {
+            Assert.AreEqual(true, LibraryUtilty.IsBookIdPowerOfTwo(128));
+            Assert.AreEqual(false, LibraryUtilty.IsBookIdPowerOfTwo(99));
+        }
+
+        [TestMethod]
+        public void CheckReverseBookTitle()
+        {
+            string title = "B1";
+            var bookTitleReversed = LibraryUtilty.ReverseBookTitle(title);
+            Assert.AreEqual(title[0], bookTitleReversed[1]);
+            Assert.AreEqual(title[1], bookTitleReversed[0]);
+        }
+
+        [TestMethod]
+        public void CheckGenerateBookTitleReplicas()
+        {
+            string title = "B1";
+            var bookTitleReplicas = LibraryUtilty.GenerateBookTitleReplicas(title, 3);
+            Assert.AreEqual(bookTitleReplicas, title + title + title);
+
+        }
+
+        [TestMethod]
+        public void CheckAllOddNumbersBetweenZeroTwoHundred()
+        {
+            var allOddNumbers = LibraryUtilty.AllOddNumberBetweenZeroToHundred();
+            Assert.AreEqual(50, allOddNumbers.Count);
+            Assert.AreEqual(1, allOddNumbers.First());
+            Assert.AreEqual(99, allOddNumbers.Last());
+        }
+    }
+}
---
+++ Rebtel.DeveloperTest.WarmUpTasks/LibraryUtilty.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace WarmUpTasks
+{
+    public static class LibraryUtilty
+    {
+        public static bool IsBookIdPowerOfTwo(int bookId)
+        {
+            return (bookId != 0) && ((bookId & (bookId - 1)) == 0);
+        }
+
+        public static string ReverseBookTitle(string title)
+        {
+            if (title == null) return null;
+            char[] charArray = title.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        public static string GenerateBookTitleReplicas(string title, int number)
+        {
+            StringBuilder sb = new StringBuilder(title);
+            for (int i = 1; i < number; i++)
+            {
+                sb.Append(title);
+            }
+
+            return sb.ToString();
+        }
+
+        public static List<int> AllOddNumberBetweenZeroToHundred()
+        {
+            return Enumerable.Range(0, 100).Where(x => x % 2 != 0).ToList();
+        }
+
+    }
+}
