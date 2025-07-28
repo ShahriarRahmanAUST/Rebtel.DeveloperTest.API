210b0d8d71ad5de107cbd8ae1e3032bc554f817d
ShahriarRahmanAUST <tiloknet@gmail.com>
2025-07-28 04:10:46 +0200

    Add Funtional Tests

---
+++ Rebtel.DeveloperTest.API.FunctionalTest/BookControllerShouldTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using Rebtel.DeveloperTest.API;
+using Rebtel.DeveloperTest.API.FunctionalTest;
+using Rebtel.DeveloperTest.BLL;
+using System.Diagnostics.CodeAnalysis;
+using System.Net.Http.Json;
+
+namespace FunctionalTest;
+[ExcludeFromCodeCoverage]
+public class BookControllerShouldTests(
+    CustomWebApiFactory factory) : IClassFixture<CustomWebApiFactory>
+{
+    [Fact]
+    public async Task ReturnExpectedBook()
+    {
+        var result = await factory.CreateClient().GetFromJsonAsync<AvaiableBook>("/api/v1/Book/GetAvailableBooks?bookId=1");       
+               
+        result.BookId.Should().Be(1);
+    }
+
+    [Fact]
+    public async Task BookNotFound()
+    {
+        var result = await factory.CreateClient().GetAsync("/api/v1/Book/GetAvailableBooks?bookId=200");
+        result.StatusCode.Equals(204);
+    }
+}
---
+++ Rebtel.DeveloperTest.API.FunctionalTest/CustomWebApiFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Rebtel.DeveloperTest.API.FunctionalTest
+{
+    public class CustomWebApiFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseEnvironment("Test");
+        }
+    }
+    
+}
